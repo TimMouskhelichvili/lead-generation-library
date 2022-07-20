34bfe92d30093e459bd9229bf0ac47769288e968
Tim Mouskhelichvili <t.mouskhelichvili@gmail.com>
2022-07-19 22:19:03 -0400

    Refactored textQuestion

--- src/pages/questions/components/types/textQuestion/index.tsx
+++ src/pages/questions/components/types/textQuestion/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import { Container } from 'src/pages/questions/components/container';
+import { IQuestion } from 'src/interfaces/IQuestion';
 import { useStore } from 'src/context';
 import { Input } from './style';
 
@@ -7,11 +8,32 @@ import { Input } from './style';
  * The TextQuestion component.
  */
 export const TextQuestion = (): ReactElement => {
-    const [ value, setValue ] = useState(''); 
     const question = useStore(c => c.question);
     const result = useStore(c => c.result);
+    const [ value, handleChange ] = useValue(question, result);
+ 
     const selected = value ? [ value ] : [];
     const disabled = !value;
+
+    return (
+        <Container title={question.title} selected={selected} disabled={disabled}>
+            <Input 
+                placeholder={question.placeholder} 
+                value={value}
+                onChange={handleChange} />
+        </Container>
+    );
+};
+
+type HandleType = (e: React.ChangeEvent<HTMLInputElement>) => void;
+
+/**
+ * The value hook.
+ * @param {IQuestion} question - The question. 
+ * @param {string[]} result - The result. 
+ */
+const useValue = (question: IQuestion, result?: string[]): [ string, HandleType ] => {
+    const [ value, setValue ] = useState(''); 
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
         setValue(e.target.value);
@@ -29,12 +51,8 @@ export const TextQuestion = (): ReactElement => {
         setValue(result[0]);
     }, [ result ]);
 
-    return (
-        <Container title={question.title} selected={selected} disabled={disabled}>
-            <Input 
-                placeholder={question.placeholder} 
-                value={value}
-                onChange={handleChange} />
-        </Container>
-    );
+    return [
+        value,
+        handleChange
+    ];
 };

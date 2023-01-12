05a35191c24ac9e64e7e4ca3b31fa4a5bfcff123
Tim Mouskhelichvili <t.mouskhelichvili@protonmail.com>
2023-01-12 00:48:32 -0500

    Fixed more issues

--- src/utils/questions.ts
+++ src/utils/questions.ts
@@ -1,7 +1,7 @@
 import { IQuestion } from 'src/interfaces/IQuestion';
 
 /**
- * Returns the filtered user questions.
+ * Returns the filtered that are counted in the total and shown in the answers screen.
  * @param {IQuestion[]} questions - The questions. 
  */
 export const getFilteredQuestions = (questions: IQuestion[]): IQuestion[] => {

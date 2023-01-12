2a686fe05f2b2cfd4d8904c94dd4e0cad9723016
Tim Mouskhelichvili <t.mouskhelichvili@protonmail.com>
2023-01-12 01:01:06 -0500

    Fixed a few things

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,11 +1,21 @@
-export const QuestionTypeArray = [ 'text' ] as const;
+export const QuestionTypeArray = [ 
+    'text' 
+] as const;
 
 export type QuestionType = typeof QuestionTypeArray[number];
 
-export const ValidateTypeArray = [ 'email' ] as const;
+export const ValidateTypeArray = [ 
+    'email' 
+] as const;
 
 export type ValidateType = typeof ValidateTypeArray[number];
 
-export const StatusArray = [ 'NOT_STARTED', 'ACTIVE', 'COMPLETED', 'SUBMITTING', 'VIEWING_ANSWERS' ] as const;
+export const StatusArray = [ 
+    'NOT_STARTED', 
+    'ACTIVE', 
+    'COMPLETED', 
+    'SUBMITTING', 
+    'VIEWING_ANSWERS' 
+] as const;
 
 export type Status = typeof StatusArray[number];
